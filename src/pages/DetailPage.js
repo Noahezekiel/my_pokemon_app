3c53f104d993338b598b571a76bfd5f9309fd02d
quit <nnooaahh24@gmail.com>
2025-05-08 11:56:39 +0100

    Adding more details

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,6 +1,7 @@
+// DetailPage.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import './DetailPage.css';
 
 const DetailPage = () => {
@@ -22,13 +23,54 @@ const DetailPage = () => {
 
   if (!pokemon) return <p>Loading...</p>;
 
+  const mainType = pokemon.types[0].type.name;
+  const colorClass = `${mainType}-bg`;
+
   return (
-    <div className="detail-page">
-      <h1>{pokemon.name}</h1>
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-      <p>Height: {pokemon.height}</p>
-      <p>Weight: {pokemon.weight}</p>
-      <p>Types: {pokemon.types.map(type => type.type.name).join(', ')}</p>
+    <div className={`detail-page ${colorClass}`}>
+      <div className="detail-header">
+        <Link to="/" className="back-button">←</Link>
+        <h1 className="pokemon-name">
+          {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+        </h1>
+        <span className="pokemon-id">#{String(pokemon.id).padStart(3, '0')}</span>
+        <img
+          className="detail-image"
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+          alt={pokemon.name}
+        />
+        <div className="types">
+          {pokemon.types.map((type, i) => (
+            <span key={i} className={`type ${type.type.name}`}>
+              {type.type.name}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <div className="detail-info">
+        <h2>Base Stats</h2>
+        <ul className="stats-list">
+          {pokemon.stats.map((stat, i) => (
+            <li key={i} className="stat-item">
+              <span className="stat-name">{stat.stat.name}</span>
+              <div className="stat-bar">
+                <div
+                  className="stat-fill"
+                  style={{ width: `${stat.base_stat / 2}%` }}
+                ></div>
+              </div>
+              <span className="stat-value">{stat.base_stat}</span>
+            </li>
+          ))}
+        </ul>
+
+        <div className="extra-info">
+          <p><strong>Height:</strong> {pokemon.height / 10} m</p>
+          <p><strong>Weight:</strong> {pokemon.weight / 10} kg</p>
+          <p><strong>Base Experience:</strong> {pokemon.base_experience}</p>
+        </div>
+      </div>
     </div>
   );
 };
